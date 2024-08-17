d7569a8c7677f70b431c88de107ede8091a5f8c5
hadeer <hadeeressam21@gmail.com>
2024-08-17 22:17:44 +0300

    + create Add New Task fragment style ready

---
+++ app/src/main/java/com/example/todoapp/appstack/AddTaskFragment.kt
@@ -0,0 +1,30 @@
+package com.example.todoapp.appstack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentAddTaskBinding
+
+class AddTaskFragment : Fragment() {
+    private lateinit var binding : FragmentAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentAddTaskBinding.inflate(inflater , container , false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
