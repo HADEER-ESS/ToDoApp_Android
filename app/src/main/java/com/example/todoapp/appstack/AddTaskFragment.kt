fa4381eae74c1b279d76674a3536cfc9c684cd0b
hadeer <hadeeressam21@gmail.com>
2024-08-23 23:13:03 +0300

    $ modify in add task fragemnt to be functional, to take tasks title, desc, and open date picker, to get task date and store it inside variable

--- app/src/main/java/com/example/todoapp/appstack/AddTaskFragment.kt
+++ app/src/main/java/com/example/todoapp/appstack/AddTaskFragment.kt
@@ -1,16 +1,31 @@
 package com.example.todoapp.appstack
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import com.example.todoapp.databinding.FragmentAddTaskBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.time.Year
+import java.util.Date
 
 class AddTaskFragment : BottomSheetDialogFragment() {
     private lateinit var binding : FragmentAddTaskBinding
+    private lateinit var taskTitle : TextInputEditText
+    private lateinit var taskDescription : TextInputEditText
+    private lateinit var taskData : AppCompatButton
+
+    private var taskName : String = ""
+    private var taskDetails : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +37,56 @@ class AddTaskFragment : BottomSheetDialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentAddTaskBinding.inflate(inflater , container , false)
+        taskTitle = binding.taskTitleInput
+        taskDescription = binding.taskDetailsInput
+        taskData = binding.selectDateBtn
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.addTaskFBtn.setOnClickListener {
             dismiss()
         }
+
+        taskTitle.setOnFocusChangeListener { v, hasFocus -> taskTitleHanlde(hasFocus) }
+
+        taskDescription.setOnFocusChangeListener { v, hasFocus -> taskDescriptionHandle(hasFocus) }
+
+        taskData.setOnClickListener { taskDateSelection() }
+    }
+
+    fun taskTitleHanlde(focus : Boolean){
+        if(!focus && taskTitle.text.isNullOrBlank()){
+            binding.taskTitleLy.error = "Please Enter Task"
+        }
+        else{
+            binding.taskTitleLy.error = null
+            taskName = taskTitle.text.toString()
+//            println("something $taskName")
+        }
+    }
+
+    fun taskDescriptionHandle(focus: Boolean){
+        if(binding.taskDetailsInput.text.isNullOrBlank()){
+            taskDetails = ""
+        }
+        else{
+            taskDetails = taskDescription.text.toString()
+        }
+    }
+
+    fun taskDateSelection(){
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
+        val datePickerDialog =  MaterialDatePicker.Builder.datePicker()
+            .setTextInputFormat(sdf)
+            .build()
+
+        datePickerDialog.addOnPositiveButtonClickListener {
+            val date = Date(it)
+            val formate = sdf.format(date)
+            println("date picker ${formate}")
+        }
+        datePickerDialog.show(parentFragmentManager , "materialDatePicker")
     }
 }
