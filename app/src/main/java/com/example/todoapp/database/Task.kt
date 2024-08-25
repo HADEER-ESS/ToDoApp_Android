7a89117603f861083d6d6e0ea21283f4cbfa430d
hadeer <hadeeressam21@gmail.com>
2024-08-25 21:56:33 +0300

    $ apply functionality on task item to navigate to Edit task screen

--- app/src/main/java/com/example/todoapp/database/Task.kt
+++ app/src/main/java/com/example/todoapp/database/Task.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp.database
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class Task(
     @PrimaryKey(autoGenerate = true) val taskId : Int,
@@ -11,4 +14,4 @@ data class Task(
     @ColumnInfo val taskDetails : String?,
     @ColumnInfo var isDone : Boolean = false,
     @ColumnInfo val date : String
-)
+) : Parcelable
